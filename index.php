a37e0ac4c0bee464a85ffa15c59a7d99eec73b3f
Andrei Pugatsov <andrei.pugatsov@gmail.com>
2017-03-28 14:56:38 +0300

    Algus

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>i244, ülesanne 7</title>
+</head>
+
+<body>
+
+<?php
+	$tekst = "Originaalne tekst"
+	echo $tekst <br>;
+	$tekstiMassiiv = str_split($tekst);
+	for ($i = count($tekstiMassiiv); $i > 0; $i = $i - 1) {
+		echo $tekstiMassiiv[$i-1];
+	}
+?>
+
+</body>
+
+</html>
